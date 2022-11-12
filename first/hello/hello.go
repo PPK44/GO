b0160ad0e0886666e5125320844c4382b69307a8
Paul <paul.kerrigan@ontariotechu.net>
2022-11-12 15:08:54 -0500

    beginning

---
+++ first/hello/hello.go
@@ -0,0 +1,19 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello, World!")
+// }
+
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Go())
+}
